736e93007daf152863e01b6fb72e62a568b0ca55
RanaSagar2004 <22051607@kiit.ac.in>
2025-04-15 22:41:14 +0530

    Add files via upload

---
+++ tictactoe_ai.py
@@ -0,0 +1,62 @@
+
+# Tic Tac Toe with Minimax AI
+import math
+
+def print_board(board):
+    for row in board:
+        print("|".join(row))
+    print()
+
+def check_winner(board):
+    for row in board:
+        if row.count(row[0]) == 3 and row[0] != ' ':
+            return row[0]
+    for col in range(3):
+        if board[0][col] == board[1][col] == board[2][col] != ' ':
+            return board[0][col]
+    if board[0][0] == board[1][1] == board[2][2] != ' ':
+        return board[0][0]
+    if board[0][2] == board[1][1] == board[2][0] != ' ':
+        return board[0][2]
+    return None
+
+def minimax(board, depth, is_maximizing):
+    winner = check_winner(board)
+    if winner == 'X': return -1
+    if winner == 'O': return 1
+    if all(cell != ' ' for row in board for cell in row): return 0
+
+    if is_maximizing:
+        best_score = -math.inf
+        for i in range(3):
+            for j in range(3):
+                if board[i][j] == ' ':
+                    board[i][j] = 'O'
+                    score = minimax(board, depth + 1, False)
+                    board[i][j] = ' '
+                    best_score = max(score, best_score)
+        return best_score
+    else:
+        best_score = math.inf
+        for i in range(3):
+            for j in range(3):
+                if board[i][j] == ' ':
+                    board[i][j] = 'X'
+                    score = minimax(board, depth + 1, True)
+                    board[i][j] = ' '
+                    best_score = min(score, best_score)
+        return best_score
+
+def best_move(board):
+    move = None
+    best_score = -math.inf
+    for i in range(3):
+        for j in range(3):
+            if board[i][j] == ' ':
+                board[i][j] = 'O'
+                score = minimax(board, 0, False)
+                board[i][j] = ' '
+                if score > best_score:
+                    best_score = score
+                    move = (i, j)
+    return move
